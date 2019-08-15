88fd2499ce51efa59504e8993ad2f6129ab990a8
aspalacios <abigail.saballos16@est.uca.edu.ni>
2019-08-14 22:44:48 -0600

    first changes

---
+++ PollApi/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using PollApi.Models;
+using System.Net;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using UserDataAccess;
+
+namespace PollApi.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/login")]
+    [EnableCors(origins: "http://localhost:8080", headers: "*", methods: "*")]
+    public class LoginController : ApiController
+    {
+        [HttpGet]
+        [Route("echoping")]
+        public IHttpActionResult EchoPing()
+        {
+            return Ok(true);
+        }
+
+        [HttpPost]
+        [Route("authenticate")]
+        public IHttpActionResult Authenticate(LoginRequest login)
+        {
+
+            if (login == null)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            UserDataEntities entities = new UserDataEntities();
+
+            var user = entities.Users.FirstOrDefault(e => e.Username == login.Username && e.Password == login.Password);
+            if (user != null)
+            {
+                var token = TokenGenerator.GenerateTokenJwt(login.Username);
+                return Ok(token);
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
---
+++ PollApi/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Linq;
+using System.Web.Http.Cors;
+using UserDataAccess;
+
+namespace PollApi.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/users")]
+    [EnableCors(origins: "http://localhost:8080", headers: "*", methods: "*")]
+    public class UsersController : ApiController
+    {
+        [HttpGet]
+        [Route("users")]
+        public IEnumerable<User> Get()
+        {
+            using (UserDataEntities entities = new UserDataEntities())
+            {
+                return entities.Users.ToList();
+            }
+        }
+    }
+}
